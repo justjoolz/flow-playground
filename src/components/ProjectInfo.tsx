55b1689535e0603876dfb0a37d88bbfbb5853b0f
justjoolz <justjoolz@gmail.com>
2021-10-08 23:07:39 +0700

    fix bug on project sidebar isActive

--- src/components/ProjectInfo.tsx
+++ src/components/ProjectInfo.tsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import {navigate} from "@reach/router"
 import {SidebarSection as Root} from "layout/SidebarSection";
 import {SidebarItems as Items} from "layout/SidebarItems";
 import {ProjectItem as Item} from "layout/ProjectItem";
 import {useProject} from "providers/Project/projectHooks";
 import { isUUUID} from "../util/url";
+import { EntityType } from "providers/Project";
 
 const ProjectInfo: React.FC = () => {
   const {
     project,
+    active
   } = useProject();
-
-  const [projectInfoSelected, setProjectInfoSelected] = useState< boolean | null>(false)
 
   const projectPath = isUUUID(project.id) ? project.id : "local"
 
@@ -19,9 +19,8 @@ const ProjectInfo: React.FC = () => {
     <Root>
       <Items>
         <Item
-          active={projectInfoSelected}
+          active={ active.type === EntityType.Readme }
           onClick={() => {
-            setProjectInfoSelected(true)
             navigate(`/${projectPath}?type=readme&id=LOCAL-account-0`)
           }}
         >
